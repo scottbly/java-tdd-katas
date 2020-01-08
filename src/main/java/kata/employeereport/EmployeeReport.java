837402ffe5eb5468461b4f9265a708ffee2b66b7
Scott Bly <bly.scott@principal.com>
2020-01-08 15:51:53 -0600

    Cleanup imports

--- src/main/java/kata/employeereport/EmployeeReport.java
+++ src/main/java/kata/employeereport/EmployeeReport.java
@@ -2,15 +2,8 @@ package kata.employeereport;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.Period;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-
-import static java.time.DayOfWeek.FRIDAY;
-import static java.util.Collections.emptyList;
-import static java.util.Collections.sort;
 
 class EmployeeReport {
 
